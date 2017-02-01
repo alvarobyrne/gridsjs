80a4c3a935704f8ec90933ccc2b3544476b81047
alvar <alvarobyrne@gmail.com>
2017-02-01 12:03:15 -0500

    moved watcher to file

--- app.js
+++ app.js
@@ -12,36 +12,6 @@ window
 console.log("window : ",window);
 // var location = window.Location;
 // console.log("location : ",location);
-
-var Watcher = require('watch-fs').Watcher;
- 
-var watcher = new Watcher({
-    paths: [ '.' ],
-    filters: {
-        includeFile: function(name) {
-        	// console.log("name : ",name);
-            return /\.js/.test(name);
-        }
-    }
-});
- 
-watcher.on('create', function(name) {
-    console.log('file ' + name + ' created');
-});
- 
-watcher.on('change', function(name) {
-    console.log('file ' + name + ' changed');
-    location.reload();
-});
- 
-watcher.on('delete', function(name) {
-    console.log('file ' + name + ' deleted');
-});
- 
-watcher.start(function(err, failed) {
-    console.log('watcher started');
-    console.log('files not found', failed);
-});
 var canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 canvas.style.border='1px solid red';
@@ -50,3 +20,4 @@ canvas.width=innerWidth;
 canvas.height=innerHeight;
 var canvasCtx = canvas.getContext('2d');
 var dt = guinw.Window.get().showDevTools();
+console.log("dt : ",dt);
---
+++ watcher.js
@@ -0,0 +1,30 @@
+
+var Watcher = require('watch-fs').Watcher;
+ 
+var watcher = new Watcher({
+    paths: [ '.','app.thml' ],
+    filters: {
+        includeFile: function(name) {
+        	// console.log("name : ",name);
+            return /\.js/.test(name);
+        }
+    }
+});
+ 
+watcher.on('create', function(name) {
+    console.log('file ' + name + ' created');
+});
+ 
+watcher.on('change', function(name) {
+    console.log('file ' + name + ' changed');
+    location.reload();
+});
+ 
+watcher.on('delete', function(name) {
+    console.log('file ' + name + ' deleted');
+});
+ 
+watcher.start(function(err, failed) {
+    console.log('watcher started');
+    console.log('files not found', failed);
+});
